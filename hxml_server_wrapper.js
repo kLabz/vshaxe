4ab3ad554be32a0d540c42430d6564935e534dd3
Simon Krajewski <simon@haxe.org>
2016-09-12 19:39:12 +0200

    sync with server

--- hxml_server_wrapper.js
+++ hxml_server_wrapper.js
@@ -1,4 +1,4 @@
 // this is needed for starting server.js with child_process directly
 // and not blocking the server.js file for writing, so we can recompile it
 // while it's running
-require("./bin/hxml_server.js");
+require("./bin/server.js").hxmlLanguageServer.Main.main();
